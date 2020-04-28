088a47f7499143ef538692c471891ccb650e8375
bishkip <64456163+bishkip@users.noreply.github.com>
2020-04-28 18:16:05 +1200

    Collapse links more obvious

--- content.js
+++ content.js
@@ -1,10 +1,16 @@
 function collapseContent(){
-    $( ".content" ).each((i, Element)=>{
+    var content = $( ".content" );
+
+    content.each((i, Element)=>{
         var id = "collapse-"+i;
 
         var sectionname = Element.querySelector(".sectionname");
         $(sectionname).attr("data-toggle", "collapse");
         $(sectionname).attr('href', '#'+id);
+        $(sectionname).attr("style", "cursor: pointer");
+        
+        $(sectionname).attr("onmouseover", "this.style.color='green'");
+        $(sectionname).attr("onmouseout", "this.style.color='black'");
 
         var section = Element.querySelector(".section");
         $(section).addClass('collapse');
